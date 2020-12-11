e168d1cc4e0b04e66ccafbb0e039542f9cd3d577
showtime996 <2461535869@qq.com>
2020-12-11 21:22:45 +0800

    commit

--- chengji-client/src/redux/reducers.js
+++ chengji-client/src/redux/reducers.js
@@ -1,13 +1,39 @@
-// 包含多个reducer函数：根据老的state和指定的action返回一个新的state 结合一个reducer
-import { combineReducers } from 'redux'
-function xxx(state=0,action){
-    return state
+/*
+包含n个reducer函数: 根据老的state和指定的action返回一个新的state
+ */
+import {combineReducers} from 'redux'
+
+import {
+  AUTH_SUCCESS,
+  ERROR_MSG,
+  RECEIVE_USER,
+  RESET_USER
+} from './action-types'
+const initUser = {
+  username: '', // 用户名
+  type: '', // 用户类型
+  msg: '', // 错误提示信息
+  // redirectTo: '' // 需要自动重定向的路由路径
 }
-function yyy(state=0,action){
-    return state
+// 产生user状态的reducer
+function user(state=initUser, action) {
+  switch (action.type) {
+    case AUTH_SUCCESS: // data是user
+      const {type, header} = action.data
+      return {...action.data, redirectTo: getRedirectTo(type, header)}
+    case ERROR_MSG: // data是msg
+      return {...state, msg: action.data}
+    case RECEIVE_USER: // data是user
+      return action.data
+    case RESET_USER: // data是msg
+      return {...initUser, msg: action.data}
+    default:
+      return state
+  }
 }
+
 export default combineReducers({
-    xxx,
-    yyy
+  user
 })
-// 向外暴露的状态结构
+
+
